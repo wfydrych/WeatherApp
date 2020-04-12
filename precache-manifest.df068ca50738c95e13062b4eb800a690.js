c8dbcb8c74b0ed60d227e261e026d6bd51490777
Wojciech Fydrych <wfydrych@gmail.com>
2020-04-12 21:24:31 +0200

    Updates

---
+++ precache-manifest.df068ca50738c95e13062b4eb800a690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa7a064fab36a5b6ca74b19c324bb5e",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "56ff116aebc39af80324",
+    "url": "/WeatherApp/static/css/main.33d1bc60.chunk.css"
+  },
+  {
+    "revision": "4913b898d9053782894f",
+    "url": "/WeatherApp/static/js/2.33143f03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.33143f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ff116aebc39af80324",
+    "url": "/WeatherApp/static/js/main.4e8dee4a.chunk.js"
+  },
+  {
+    "revision": "4583da910e0b66774bc3",
+    "url": "/WeatherApp/static/js/runtime-main.795e39ca.js"
+  }
+]);
